7001ad5c47c7ccd72d73e0987530922056d56398
AzizDeveloper04 <saparbaev0131@gmail.com>
2023-08-14 17:02:08 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Please enter firstnumber: ");
+int a=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Please enter secondnumber: ");
+int b=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"{a}+{b}={a+b}");
+System.Console.WriteLine($"{a}-{b}={a-b}");
+System.Console.WriteLine($"{a}*{b}={a*b}");
+System.Console.WriteLine($"{a}/{b}={a/b}");
